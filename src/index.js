0b55eef842fcbcc264d606a8a1f1eaa356079106
Alex Banks <alex@moonhighway.com>
2018-06-29 19:21:59 -0700

    First Production Build
    Changes
    * Displays the current endpoint in the console.logs
    * removes dot env
    * Adds welcome message

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import { ApolloProvider } from 'react-apollo'
 const uri = process.env.REACT_APP_GRAPHQL_ENDPOINT || 'http://localhost:4000'
 const client = new ApolloClient({uri})
 
+console.log('%cwelcome to high fives', 'color: purple; font-size: 40px')
+console.log('%cthank you so much for your help!!!', 'color: blue; font-size: 20px')
+console.log('connected to graphql endpoint ', uri)
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
